4f6e57231c50899c1ee8d1160191716d4e29b783
glorious-communicator-0033 <clinton.weaver@gmail.com>
2020-10-25 23:50:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ function increaseRankBy(n) {
     .querySelectorAll('ul.ranked-list li');
 
   for(let i = 0; i < lis.length; i++) {
-    console.log(lis[i])
-    j = parseInt(lis[i]) + 1;
+    console.log(lis[i].innerHTML])
+    j = parseInt(lis[i].innerHTML) + 1;
     lis[i].innerHTML = (j).toString();
   }
 
